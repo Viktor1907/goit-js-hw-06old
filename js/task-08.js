31cec796631125c81fe869ad792c31de245fe248
Viktor1907 <vikvelik@bigmir.net>
2023-11-06 18:27:13 +0200

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(loginForm);
+  const formObjeect = {};
+
+  formData.forEach((value, key) => {
+    formObjeect[key] = value;
+  });
+  if (!formObjeect.email || !formObjeect.password) {
+    alert("Всі поля повинні бути заповненні");
+  } else {
+    console.log(formObjeect);
+    loginForm.requestFullscreen();
+  }
+});
